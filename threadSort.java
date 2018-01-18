60c5e88c8a214b2d357ba064cbfd17d3e0dd42dc
Aly Mohamed Aly <mo7tarif1@hotmail.com>
2018-01-19 00:02:02 +0200

    removed unnecessaryattributes, better sorting algorithm

--- threadSort.java
+++ threadSort.java
@@ -1,4 +1,4 @@
-package OS;
+package MultiThreadedSort;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -6,84 +6,9 @@ import java.util.Random;
 public class threadSort{
 
     public static void main (String[] args){
-        int[] arr = {15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};             //Worst case
-        System.out.println("Test_1: " + Arrays.toString(arr));
-        //arr = sortThis(arr);
-        System.out.println("Result_1: " + Arrays.toString(sortThis(arr)));
-
-        System.out.println("------------------------------------------------------------");
-
-        int[] arr2 = {1, 2, 3, 4, 5, 11, 5, 9, 8, 7, 6, 5, 5, 3, 22, 1};             //Sorted first quarter
-        System.out.println("Test_2: " + Arrays.toString(arr2));
-        arr2 = sortThis(arr2);
-        System.out.println("Result_2: " + Arrays.toString(arr2));
-
-        System.out.println("------------------------------------------------------------");
-
-        int[] arr3 = {1, 2, 3};                                                     //Already sorted
-        System.out.println("Test_3: " + Arrays.toString(arr3));
-        arr3 = sortThis(arr3);
-        System.out.println("Result_3: " + Arrays.toString(arr3));
-
-        System.out.println("------------------------------------------------------------");
-
-        int[] arr4 = {4, 2, 3, 1};                                                   //Small array
-        System.out.println("Test_4: " + Arrays.toString(arr4));
-        arr4 = sortThis(arr4);
-        System.out.println("Result_4: " + Arrays.toString(arr4));
-
-        System.out.println("------------------------------------------------------------");
-
-        int[] arr5 = {1};                                                           //One int array
-        System.out.println("Test_5: " + Arrays.toString(arr5));
-        arr5 = sortThis(arr5);
-        System.out.println("Result_5: " + Arrays.toString(arr5));
-
-        System.out.println("------------------------------------------------------------");
-
-        int[] arr6 = {};                                                            //Empty array
-        System.out.println("Test_6: " + Arrays.toString(arr6));
-        arr6 = sortThis(arr6);
-        System.out.println("Result_6: " + Arrays.toString(arr6));
-
-        System.out.println("------------------------------------------------------------");
-
-        int[] arr7 = {66, 66, 66, 66, 66, 66, 66, 66};                              //Repeated number array
-        System.out.println("Test_7: " + Arrays.toString(arr7));
-        arr7 = sortThis(arr7);
-        System.out.println("Result_7: " + Arrays.toString(arr7));
-        System.out.println("------------------------------------------------------------");
-
-        Random r = new Random();
-        int[] arr8 = new int[24];                                                  //Random array
-        for (int i = 0; i < 24; i++){
-            arr8[i] = r.nextInt(i * 3 + 4);
-        }
-        System.out.println("Test_8: " + Arrays.toString(arr8));
-        arr8 = sortThis(arr8);
-        System.out.println("Result_8: " + Arrays.toString(arr8));
-        System.out.println("------------------------------------------------------------");
-
-        int[] arr9 = new int[24];                                                  //Random mostly negative array
-        for (int i = 0; i < 24; i++){
-            arr9[i] = r.nextInt(i * 3 + 4) - 60;
-        }
-        System.out.println("Test_9: " + Arrays.toString(arr9));
-        arr9 = sortThis(arr9);
-        System.out.println("Result_9: " + Arrays.toString(arr9));
-        System.out.println("------------------------------------------------------------");
-
-        int[] arr10 = new int[80];                                                  //Big random array
-        for (int i = 0; i < 80; i++){
-            arr10[i] = r.nextInt(i * 3 + 4) - 60;
-        }
-        System.out.println("Test_10: " + Arrays.toString(arr10));
-        arr10 = sortThis(arr10);
-        System.out.println("Result_10: " + Arrays.toString(arr10));
-        System.out.println("------------------------------------------------------------");
     }
 
-    public static int[] mergeThis (int[] ar1, int[] ar2, int sz){
+    public static int[] MergeThis (int[] ar1, int[] ar2, int sz){
         int[] ans = new int[sz];
         int i, j, k;
         for (i = 0, j = 0, k = 0; i < ar1.length && j < ar2.length;){
@@ -111,13 +36,9 @@ public class threadSort{
     public static class SortingThread extends Thread{
 
         private int[] ar;
-        private int start;
-        private int end;
 
         public SortingThread (int[] arr, int start, int end){
             ar = Arrays.copyOfRange(arr, start, end);
-            this.start = start;
-            this.end = end;
         }
 
         public SortingThread (){ //for empty instialization
@@ -127,15 +48,7 @@ public class threadSort{
         public void run (){
             if (ar.length < 2)
                 return;
-            for (int i = 0; i < ar.length; i++){
-                for (int j = 0; j < ar.length - 1; j++){
-                    if (ar[j] > ar[j + 1]){
-                        int temp = ar[j];
-                        ar[j] = ar[j + 1];
-                        ar[j + 1] = temp;
-                    }
-                }
-            }
+            Arrays.sort(ar);
         }
     }
 
@@ -247,14 +160,16 @@ public class threadSort{
         MergingThread MH4 = new MergingThread();  //empty instialization
         MergingThread MH5 = new MergingThread();  //empty instialization
         MergingThread MH6 = new MergingThread();  //empty instialization
-        while (true){
-            switch (state){
-            case 2:
-                if (!TH1.isAlive() && !TH2.isAlive()){
-                    return mergeThis(TH1.ar, TH2.ar, arr.length);
-                }
-                break;
-            case 4:
+        switch (state){
+        case 2:
+            try{
+                TH1.join();
+                TH2.join();
+            }catch (InterruptedException ex){
+            }
+            return MergeThis(TH1.ar, TH2.ar, arr.length);
+        case 4:
+            while (true)
                 if ((!TH1.isAlive() && !TH2.isAlive()) || (!TH3.isAlive() && !TH4.isAlive())){
                     boolean flag1 = true;
                     boolean flag2 = true;
@@ -267,28 +182,30 @@ public class threadSort{
                             }
                         }
                         if (flag2){
-                            if (!TH1.isAlive() && !TH2.isAlive()){
+                            if (!TH3.isAlive() && !TH4.isAlive()){
                                 MH2 = new MergingThread(TH3.ar, TH4.ar, arr.length - (arr.length / 2));
                                 MH2.run();
                                 flag2 = false;
                             }
                         }
                         if (!flag1 && !flag2){
-                            if (!MH1.isAlive() && !MH2.isAlive()){
-                                return mergeThis(MH1.ans, MH2.ans, arr.length);
+                            try{
+                                MH1.join();
+                                MH2.join();
+                            }catch (InterruptedException ex){
                             }
+                            return MergeThis(MH1.ans, MH2.ans, arr.length);
                         }
                     }
                 }
-                break;
-            case 8:
+        case 8:
+            while (true){
                 if ((!TH1.isAlive() && !TH2.isAlive()) || (!TH3.isAlive() && !TH4.isAlive())
                         || (!TH5.isAlive() && !TH6.isAlive()) || (!TH7.isAlive() && !TH8.isAlive())){
                     boolean flag1 = true;
                     boolean flag2 = true;
                     boolean flag3 = true;
                     boolean flag4 = true;
-
                     while (true){
                         if (flag1){
                             if (!TH1.isAlive() && !TH2.isAlive()){
@@ -339,11 +256,12 @@ public class threadSort{
                                             }
                                         }
                                         if (!flag5 && !flag6){
-                                            while (true){
-                                                if (!MH5.isAlive() && !MH6.isAlive()){
-                                                    return mergeThis(MH6.ans, MH5.ans, arr.length);
-                                                }
+                                            try{
+                                                MH5.join();
+                                                MH6.join();
+                                            }catch (InterruptedException ex){
                                             }
+                                            return MergeThis(MH6.ans, MH5.ans, arr.length);
                                         }
                                     }
                                 }
@@ -351,9 +269,8 @@ public class threadSort{
                         }
                     }
                 }
-                break;
             }
         }
+        throw new RuntimeException(); // shouldn't reach This
     }
-
 }
